fb4cfa8d6d1ae87c4a455ed162a3276d7143945a
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2020-04-03 14:03:56 +0000

    colore rect e circ

--- angular-maps/src/app/app.component.ts
+++ angular-maps/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Point } from 'src/models/point.model';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,35 @@ export class AppComponent {
   lat: number = 45.506738;
   lng: number = 9.190766;
 
+  colorCerchio:string;
+  colorRetta:string;
+  constructor(){}
+    redColor(): boolean{
+      this.colorCerchio="red"
+      return false
+    }
+    greenColor() : boolean{
+      this.colorCerchio="green"
+      return false
+    }
+    blueColor() : boolean{
+      this.colorCerchio="blue"
+      return false
+    }
+
+    violetColorett() : boolean{
+      this.colorRetta="Purple"
+      return false
+    }
+    yellowColorRett() : boolean{
+      this.colorRetta="yellow"
+      return false
+    }
+    orangeColorRett() : boolean{
+      this.colorRetta= "orange"
+      return false
+    }
+
    icon =  {
     url:'./assets/img/cat_acrobat.ico',
     scaledSize: {
@@ -18,5 +48,13 @@ export class AppComponent {
       height: 60
     }
   };
+
+   triangle: Array<Point> =
+  [
+    {lng:9.1, lat:45.5},
+    {lng:9.0, lat:45.6},
+    {lng:9.0, lat:45.4}
+  ]
+
 
 }
